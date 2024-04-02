f78e273d7101ac00eb43a47098ec1a6e33f4f2ca
Shahriyor557 <shahriyorabreyev@gmail.com>
2024-04-02 18:05:22 +0500

    finish 18:04

--- js/main.js
+++ js/main.js
@@ -6,24 +6,61 @@ let dayText = document.querySelector(".daysText");
 let monthText = document.querySelector(".monthsText");
 let yearText = document.querySelector(".yearsText");
 
-button.addEventListener("click", () => {
 
+button.addEventListener("click", () => {
     let d = Number(day.value);
     let m = Number(month.value);
     let y = Number(year.value);
-    let DedlineDate = new Date(y, m-1 , d+1);
-    let now = new Date();
-    let time = Date.parse(DedlineDate) - Date.parse(now)
-    let days = Math.floor(time / (1000 * 60 * 60 * 24) % 30);
-    let months = Math.floor(time / (1000 * 60 * 60 * 24 * 30) % 12);
-    let years = Math.floor(time / (1000 * 60 * 60 * 24 * 30 * 12));
-    dayText.innerHTML = addZero(days);
-    monthText.innerHTML = addZero(months);
-    yearText.innerHTML = addZero(years);
-    
 
+    if (d <= 0 || d > 31) {
+        day.nextElementSibling.innerHTML = "Must be a valid day";
+        day.style.border = "1px solid red";
+        
+        return; 
+    }
+    else if(m<= 0 || m > 12) {
+        month.nextElementSibling.innerHTML = "Must be a valid month";
+        month.style.border = "1px solid red";
+        return;
+    }
+    else if (y <= 0) {
+        year.nextElementSibling.innerHTML = "Must be a valid year";
+        year.style.border = "1px solid red";
+        return;
+    }
+    else {
+        day.style.border = "1px solid green";
+        month.style.border = "1px solid green";
+        year.style.border = "1px solid green";
+        day.nextElementSibling.innerHTML = "";
+        month.nextElementSibling.innerHTML = "";
+        year.nextElementSibling.innerHTML = "";
+    }
+
+   
+    let DedlineDate = new Date(y, m - 1, d + 1);
+    let now = new Date();
+    let time =  Date.parse(now) -Date.parse(DedlineDate) 
+    if (time < 0) {
+        dayText.innerHTML = "00";
+        monthText.innerHTML = "00";
+        yearText.innerHTML = "00";
+        alert("Dedline date is passed")
+
+    }
+    else {
+        let days = Math.floor(time / (1000 * 60 * 60 * 24) % 30);
+        let months = Math.floor(time / (1000 * 60 * 60 * 24 * 30) % 12);
+        let years = Math.floor(time / (1000 * 60 * 60 * 24 * 30 * 12));
+        dayText.innerHTML = addZero(days);
+        monthText.innerHTML = addZero(months);
+        yearText.innerHTML = addZero(years);
+
+    }
 
 })
+
+
 function addZero(num) {
     if (num < 10 && num >= 0) {
         return `0${num}`
